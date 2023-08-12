dd6dcb15160de6bae0efd5b6cd5eed596cb1d439
UNG <rnwldnd7248@gmail.com>
2023-08-12 16:40:27 +0900

    add: add the frame of the service
    add the order-service frame. there is valdiation checker which is considered abstraction. and buy feature is added. but not handled about cancel and track. these features are ambiguous

---
+++ src/main/java/app/error/ErrorCode.java
@@ -0,0 +1,31 @@
+package app.error;
+
+import java.util.Arrays;
+import lombok.Getter;
+
+
+@Getter
+public enum ErrorCode {
+  //@formatter:off
+ QUANTITY_IS_NOT_SUFFICIENT(400,"0001",null),
+ CUSTOMER_IS_NOT_AFFORDABLE(400,"0002",null),
+ NO_PRODUCT_IN_CART(400,"0003",null),
+ ADDRESS_IS_UNAVAILABLE(400,"0004",null);
+
+  private final int status;
+  private final String code;
+  private final String message;
+  //@formatter:on
+  ErrorCode(int status, String code, String message) {
+    this.status = status;
+    this.code = code;
+    this.message = message;
+  }
+
+  public static ErrorCode valueOfCode(String errorCode) {
+    return Arrays.stream(values())
+        .filter(value -> value.code.equals(errorCode))
+        .findAny()
+        .orElse(null);
+  }
+}
---
+++ src/main/java/app/service/order/BuyProductDirectlyChecker.java
@@ -0,0 +1,50 @@
+package app.service.order;
+
+import app.dto.OrderProcessDto;
+import app.error.ErrorCode;
+import java.util.HashMap;
+import java.util.Map;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class BuyProductDirectlyChecker implements BuyableChecker {
+
+  Map<ErrorCode,Boolean> errorMessageBooleanMap = new HashMap<>();
+  @Override
+  public Map<ErrorCode, Boolean> process(OrderProcessDto orderProcessDto) {
+
+  if (!this.isAffordable(orderProcessDto)) {
+      errorMessageBooleanMap.put(ErrorCode.CUSTOMER_IS_NOT_AFFORDABLE, Boolean.FALSE);
+    }
+    if (!this.isDeliverable(orderProcessDto)) {
+      errorMessageBooleanMap.put(ErrorCode.ADDRESS_IS_UNAVAILABLE, Boolean.FALSE);
+    }
+    if (!this.isQuantitySufficient(orderProcessDto)) {
+      errorMessageBooleanMap.put(ErrorCode.QUANTITY_IS_NOT_SUFFICIENT, Boolean.FALSE);
+    }
+    return errorMessageBooleanMap;
+
+}
+
+  @Override
+  public boolean isAffordable(OrderProcessDto orderProcessDto) {
+    return false;
+  }
+
+  @Override
+  public boolean isQuantitySufficient(OrderProcessDto orderProcessDto) {
+    return false;
+  }
+
+  @Override
+  public boolean isDeliverable(OrderProcessDto orderProcessDto) {
+    return false;
+  }
+}
