c01c7c09418913cf97105a1506fe68753649c5a6
nowgnas <dev.nowgnas@gmail.com>
2023-08-18 12:01:42 +0900

    add: product list item for like

---
+++ src/main/java/app/dto/product/ProductListItemOfLike.java
@@ -0,0 +1,20 @@
+package app.dto.product;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
+public class ProductListItemOfLike {
+  private Long id;
+  private String name;
+  private Long price;
+  private String url;
+}
