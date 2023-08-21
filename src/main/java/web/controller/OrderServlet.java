007978ffcddf127e10cdecf1b3f1f1cf5122dacb
nowgnas <dev.nowgnas@gmail.com>
2023-08-21 23:50:26 +0900

    add: add product detail page

--- src/main/java/web/controller/OrderServlet.java
+++ src/main/java/web/controller/OrderServlet.java
@@ -9,18 +9,16 @@ import app.dto.response.ProductOrderDto;
 import app.entity.Order;
 import app.service.order.OrderServiceImpl;
 import app.utils.HttpUtil;
-import web.dispatcher.Navi;
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import web.dispatcher.Navi;
 
 /** Servlet implementation class CustServlet */
 @WebServlet({"/order"})
