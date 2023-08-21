81a96ff3bca5b9fb8660befe1ec65a49f3e8eb22
JunYong Sun <ssjy4974@naver.com>
2023-08-21 11:58:31 +0900

    test : countByEmail 테스트 추가

--- src/test/java/app/dao/member/MemberDaoTest.java
+++ src/test/java/app/dao/member/MemberDaoTest.java
@@ -4,7 +4,10 @@ import app.dto.response.OrderMemberDetail;
 import config.TestConfig;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import utils.GetSessionFactory;
 
 class MemberDaoTest {
@@ -20,12 +23,6 @@ class MemberDaoTest {
     testConfig.init("init-data.sql", session);
   }
 
-  @AfterEach
-  void afterEach() throws Exception {
-    SqlSession session = sessionFactory.openSession();
-    testConfig.init("clear-data.sql", session);
-  }
-
   @Test
   @DisplayName("주문 시 멤버와 관련된 기본 주소지 정보, 쿠폰 정보(리스트)를 같이 조회 한다.")
   void select() throws Exception {
@@ -37,5 +34,35 @@ class MemberDaoTest {
 
     // then
     Assertions.assertEquals(1L, orderMemberDetail.getId());
+  }
+
+  @Test
+  @DisplayName("입력받은 이메일 데이터베이스에 존재하는지 검사한다. 존재하면 1을 반환한다.")
+  void countByEmail_result_1() throws Exception {
+    // given
+    SqlSession session = sessionFactory.openSession();
+    String email = "test@naver.com";
+    int expectedResult = 1;
+
+    // when
+    int result = memberDao.countByEmail(email, session);
+
+    // then
+    Assertions.assertEquals(expectedResult, result);
+  }
+
+  @Test
+  @DisplayName("입력받은 이메일 데이터베이스에 존재하는지 검사한다. 존재 하지 않으면 0을 반환한다.")
+  void countByEmail_result_0() throws Exception {
+    // given
+    SqlSession session = sessionFactory.openSession();
+    String email = "test123@naver.com";
+    int expectedResult = 0;
+
+    // when
+    int result = memberDao.countByEmail(email, session);
+
+    // then
+    Assertions.assertEquals(expectedResult, result);
   }
 }
