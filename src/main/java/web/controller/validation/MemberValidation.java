8135e8b248847122878deffde9cb70c4515fc730
JunYong Sun <ssjy4974@naver.com>
2023-08-21 10:24:46 +0900

    feat : MemberValidation -> isValidPassword 추가

--- src/main/java/web/controller/validation/MemberValidation.java
+++ src/main/java/web/controller/validation/MemberValidation.java
@@ -4,15 +4,25 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public abstract class MemberValidation {
-    private static final String EMAIL_REGEX =
-            "^[a-zA-Z0-9_+&*-]+(?:\\." +
-                    "[a-zA-Z0-9_+&*-]+)*@" +
-                    "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
-                    "A-Z]{2,5}$";
 
-    public static boolean isValidEmail(String email) {
-        Pattern pattern = Pattern.compile(EMAIL_REGEX);
-        Matcher matcher = pattern.matcher(email);
-        return matcher.matches();
-    }
+  private static final String EMAIL_REGEX =
+      "^[a-zA-Z0-9_+&*-]+(?:\\."
+          + "[a-zA-Z0-9_+&*-]+)*@"
+          + "(?:[a-zA-Z0-9-]+\\.)+[a-z"
+          + "A-Z]{2,5}$";
+
+  private static final String PASSWORD_REGEX =
+      "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d~!@#$%^&*()+|=]{8,16}$";
+
+  public static boolean isValidEmail(String email) {
+    Pattern pattern = Pattern.compile(EMAIL_REGEX);
+    Matcher matcher = pattern.matcher(email);
+    return matcher.matches();
+  }
+
+  public static boolean isValidPassword(String password) {
+    Pattern pattern = Pattern.compile(PASSWORD_REGEX);
+    Matcher matcher = pattern.matcher(password);
+    return matcher.matches();
+  }
 }
