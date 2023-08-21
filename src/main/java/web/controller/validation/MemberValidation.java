6b2191eaf6233ebd869d49d72b7268f04c88eed3
JunYong Sun <ssjy4974@naver.com>
2023-08-21 10:43:09 +0900

    feat : MemberValidation -> isValidName 추가

--- src/main/java/web/controller/validation/MemberValidation.java
+++ src/main/java/web/controller/validation/MemberValidation.java
@@ -14,6 +14,9 @@ public abstract class MemberValidation {
   private static final String PASSWORD_REGEX =
       "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d~!@#$%^&*()+|=]{8,16}$";
 
+  private static final String NAME_REGEX =
+          "^[ㄱ-ㅎ가-힣]{1,20}$";
+
   public static boolean isValidEmail(String email) {
     Pattern pattern = Pattern.compile(EMAIL_REGEX);
     Matcher matcher = pattern.matcher(email);
@@ -23,6 +26,12 @@ public abstract class MemberValidation {
   public static boolean isValidPassword(String password) {
     Pattern pattern = Pattern.compile(PASSWORD_REGEX);
     Matcher matcher = pattern.matcher(password);
+    return matcher.matches();
+  }
+
+  public static boolean isValidName(String name) {
+    Pattern pattern = Pattern.compile(NAME_REGEX);
+    Matcher matcher = pattern.matcher(name);
     return matcher.matches();
   }
 }
