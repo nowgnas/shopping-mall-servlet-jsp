84847ba61c7053430eb607b5881d405739df8ed7
MJun111 <kmj0568@gmail.com>
2023-08-23 23:37:38 +0900

    refactor: session factory update

--- src/main/java/app/service/likes/ProductLikesService.java
+++ src/main/java/app/service/likes/ProductLikesService.java
@@ -1,6 +1,5 @@
 package app.service.likes;
 
-import app.dao.DaoFrame;
 import app.dao.likes.LikesDao;
 import app.dao.likes.LikesDaoFrame;
 import app.dao.product.ProductDao;
@@ -13,26 +12,34 @@ import app.error.CustomException;
 import app.error.ErrorCode;
 import app.utils.GetSessionFactory;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
+
 import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 
 public class ProductLikesService implements LikesService {
-
-  LikesDaoFrame<ProductAndMemberCompositeKey, Likes> likesDao;
-  ProductDaoFrame<Long, Product> productDao;
-  SqlSession session;
+  private Logger log = Logger.getLogger("Likes Service");
+  private final LikesDaoFrame<ProductAndMemberCompositeKey, Likes> likesDao;
+  private final ProductDaoFrame<Long, Product> productDao;
+  private final SqlSessionFactory sessionFactory = GetSessionFactory.getInstance();
+  private SqlSession session;
 
   public ProductLikesService() {
     likesDao = LikesDao.getInstance();
     productDao = ProductDao.getInstance();
   }
 
+  /**
+   * 회원의 찜한 상품 목록 불러오기
+   * @param memberId
+   * @return
+   */
   @Override
   public List<ProductListItemOfLike> getMemberLikes(Long memberId) {
     List<ProductListItemOfLike> memberLikesList;
     try {
-      session = GetSessionFactory.getInstance().openSession();
+      session = sessionFactory.openSession();
       List<Long> productIdList = likesDao.selectAllProduct(memberId, session);
       memberLikesList = productDao.selectProductListItemOfLike(productIdList, session);
     } catch (Exception e) {
@@ -50,7 +57,7 @@ public class ProductLikesService implements LikesService {
   public boolean getMemberProductLikes(ProductAndMemberCompositeKey productAndMemberCompositeKey) {
     Likes likes = null;
     try {
-      session = GetSessionFactory.getInstance().openSession();
+      session = sessionFactory.openSession();
       likes = likesDao.selectById(productAndMemberCompositeKey, session).orElse(null);
     } catch (SQLException e) {
 
@@ -70,7 +77,7 @@ public class ProductLikesService implements LikesService {
   public int addLikes(ProductAndMemberCompositeKey productAndMemberCompositeKey) {
     int res = 0;
     try {
-      session = GetSessionFactory.getInstance().openSession();
+      session = sessionFactory.openSession();
       res = likesDao.insert(
           new Likes(
               productAndMemberCompositeKey.getMemberId()
@@ -97,7 +104,7 @@ public class ProductLikesService implements LikesService {
   public int removeLikes(ProductAndMemberCompositeKey productAndMemberCompositeKey) {
     int res = 0;
     try {
-      session = GetSessionFactory.getInstance().openSession();
+      session = sessionFactory.openSession();
       res = likesDao.deleteById(productAndMemberCompositeKey, session);
       session.commit();
     } catch (SQLException e) {
@@ -117,7 +124,7 @@ public class ProductLikesService implements LikesService {
   public int removeSomeLikes(List<ProductAndMemberCompositeKey> keyList) {
     int res = 0;
     try {
-      session = GetSessionFactory.getInstance().openSession();
+      session = sessionFactory.openSession();
       for (ProductAndMemberCompositeKey key : keyList) {
         res = likesDao.deleteById(key, session) > 0 ? res + 1 : res;
       }
