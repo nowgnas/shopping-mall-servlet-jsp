2b26776ad3d4c2d792362de4e9e65ca8af57875b
UNG <rnwldnd7248@gmail.com>
2023-08-24 17:58:56 +0900

    add: add the cart-servlet

--- src/main/java/web/controller/CartServlet.java
+++ src/main/java/web/controller/CartServlet.java
@@ -1,12 +1,30 @@
 package web.controller;
 
+import app.dao.cart.CartDao;
+import app.dao.cart.CartDaoFrame;
+import app.dao.member.MemberDao;
+import app.dao.member.MemberDaoFrame;
+import app.dao.product.ProductDao;
 import app.dto.cart.AllCartProductInfoDto;
+import app.entity.Cart;
+import app.entity.Member;
+import app.entity.Product;
 import app.entity.ProductAndMemberCompositeKey;
 import app.exception.ErrorCode;
 import app.exception.cart.CartNotFoundException;
 import app.exception.member.MemberNotFoundException;
 import app.exception.product.ProductNotFoundException;
 import app.service.cart.CartService;
+import app.service.cart.CartServiceImpl;
+import app.service.cart.DeleteCartWhenRestOfQuantityUnder0;
+import app.service.cart.UpdateCartService;
+import app.service.cart.UpdateCartServiceImpl;
+import app.service.checker.CartExistCheckerService;
+import app.service.checker.EntityExistCheckerService;
+import app.service.checker.MemberExistCheckerService;
+import app.service.checker.ProductExistCheckerService;
+import app.service.product.StockCheckerService;
+import app.service.product.StockCheckerServiceImpl;
 import app.utils.HttpUtil;
 import java.io.IOException;
 import javax.servlet.ServletException;
@@ -15,23 +33,37 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
-@AllArgsConstructor
-@WebServlet({"/shopping-cart"})
+@NoArgsConstructor
+@WebServlet({"/cart"})
 public class CartServlet extends HttpServlet {
 
-  private final CartService cartService;
+ private final CartDaoFrame<ProductAndMemberCompositeKey, Cart> cartDaoFrame = new CartDao();
+  private final MemberDaoFrame<Long, Member> memberDao = new MemberDao();
+  private final EntityExistCheckerService<Long, Member> memberExistCheckerService = new MemberExistCheckerService(
+      memberDao);
+  private final EntityExistCheckerService<Long, Product> productExistCheckerService = new ProductExistCheckerService();
+  private final EntityExistCheckerService<ProductAndMemberCompositeKey, Cart> cartExistCheckerService = new CartExistCheckerService();
+  private final UpdateCartService updateCartService = new UpdateCartServiceImpl(cartDaoFrame,
+      new DeleteCartWhenRestOfQuantityUnder0(cartDaoFrame));
+    private final StockCheckerService stockCheckerService = new StockCheckerServiceImpl();
+  private final CartService cartService = new CartServiceImpl(cartDaoFrame, memberDao,
+      memberExistCheckerService, productExistCheckerService, cartExistCheckerService,
+      stockCheckerService, updateCartService);
 
 
   @Override
   protected void doGet(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
-    Long memberId = (Long) request.getSession().getAttribute("memberId");
+//    Long memberId = (Long) request.getSession().getAttribute("memberId");
+    Long memberId = 1L;
 
     try {
+
       AllCartProductInfoDto cartInfo = cartService.getCartProductListByMember(memberId);
       request.setAttribute("cartInfo", cartInfo);
-      request.getRequestDispatcher("cart.jsp").forward(request, response);
+      request.getRequestDispatcher("/cart.jsp").forward(request, response);
     } catch (MemberNotFoundException e) {
       throw new MemberNotFoundException(ErrorCode.MEMBER_NOT_FOUND);
     } catch (Exception e) {
